042e7002fc8c5725b91c0ef6510b93444651f89e
ramzess <karakoom@yandex.ru>
2020-07-16 19:01:56 +0300

    вторая итерация JS

--- script/idex.js
+++ script/idex.js
@@ -7,24 +7,29 @@ let modalProfession = document.querySelector(".modal__input_profession");
 let modalName = document.querySelector(".modal__input_name");
 let submitButton = document.querySelector(".modal__form");
 
-editButton.addEventListener("click", modalToggle);
-closeModal.addEventListener("click", modalToggle);
-submitButton.addEventListener("submit", modalSave);
- 
 /* функция открытия закрытия модалки и чтения данных из профиля*/ 
 function modalToggle(){  
-    modal.classList.toggle("modal_open");
-    modalName.value = profileName.textContent;
-    modalProfession.value = profileProfession.textContent;
-}
+/* если у modal есть модификатор modal_open, убираем его */     
+    if (modal.classList.contains("modal_open")) {
+        modal.classList.toggle("modal_open");
+    }
+/* иначе, добавляем его и пишем в форму данные из секции profile */ 
+       else{
+        modal.classList.toggle("modal_open"); 
+        modalName.value = profileName.textContent;
+        modalProfession.value = profileProfession.textContent;
+        }
+    } 
 
 /* записываем данные из формы в профиль */
-function modalSave(){
+function modalSave(event){
     event.preventDefault(); /*чтобы страница не перезагружалась при нажатии на кнопку */
     profileName.textContent = modalName.value;
     profileProfession.textContent = modalProfession.value;
     modalToggle(); /* записали, а теперь закрыть окно */
 }
-
+editButton.addEventListener("click", modalToggle);
+closeModal.addEventListener("click", modalToggle);
+submitButton.addEventListener("submit", modalSave);
 
 
