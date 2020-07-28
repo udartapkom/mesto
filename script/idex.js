c0477fd6d2e57af8f79f016c91443b463820ff18
ramzess <karakoom@ya.ru>
2020-07-29 00:29:50 +0300

    complete job

--- script/idex.js
+++ script/idex.js
@@ -4,27 +4,33 @@ const profileProfession = document.querySelector(".profile-info__subtitle");
 const modalProfession = document.querySelector(".modal__input_profession");
 const modalName = document.querySelector(".modal__input_name");
 
+//Карточки
+const cards = document.querySelector(".elements");
+const cardTemplate = document.querySelector(".template-card");
+
 //окна
 const modalProfile = document.querySelector(".modal_profile");
 const modalAddCard = document.querySelector(".modal_add-card");
+const modalLookPhoto = document.querySelector(".modal_look-photo");
+
+const modalPhoto = modalLookPhoto.querySelector(".modal__photo-zoom"); 
+const modalSignature = modalLookPhoto.querySelector(".modal__photo-signature");
+const modalContent = modalLookPhoto.querySelector(".modal__content");
 
 //кнопки 
 const closeModalProfile = modalProfile.querySelector(".modal__close-button");
 const closeModalAddCard = modalAddCard.querySelector(".modal__close-button");
+const closeModalLook = modalLookPhoto.querySelector(".modal__close-button");
 
 const newCardButton = document.querySelector(".profile__add-button");
 const editProfileButton = document.querySelector(".profile-info__edit-button");
 
-const saveProfile = modalProfile.querySelector(".modal__save");
-const saveCard = modalAddCard.querySelector(".modal__save");
+const saveProfile = modalProfile.querySelector(".modal__form");
+const saveCard = modalAddCard.querySelector(".modal__form");
 
 //Поля ввода
 const modalPlace = modalAddCard.querySelector(".modal__input_profession");
 const modalTitle= modalAddCard.querySelector(".modal__input_name");
-
-//Карточки
-const cards = document.querySelector(".elements");
-const cardTemplate = document.querySelector(".template-card");
 
 const initialCards = [
     {
@@ -62,14 +68,30 @@ const cardsList = () => {
 // Функция генерирует готовую карточку в зависимости от переданных ей данных и вешает слушателей на кнопки
 const cardsGenerate = data => {
     const elCard = cardTemplate.content.cloneNode(true); // Клонируем карточку 
-    elCard.querySelector(".cards__title-style").innerText = data.name; // ищем нужный элемент и пишем в него соответствующие данные
-    elCard.querySelector(".cards__photo").src = data.link;
+    nameText = elCard.querySelector(".cards__title-style") // ищем нужные элементы
+    photoCard = elCard.querySelector(".cards__photo");
+    nameText.innerText = data.name;  //и пишем в них соответствующие данные
+    photoCard.src = data.link; 
+    photoCard.alt = data.name;
+    findFunction(elCard, nameText, photoCard); //передаем параметры для слушателей и модалки
+    return elCard;
+}
+// Поиск и навешивание слушателей
+function findFunction(elCard, nameText, photoCard ){
     const like = elCard.querySelector(".cards__like"); // ищем кнопку лайк и корзина
     const trashButton = elCard.querySelector(".cards__trash"); 
     trashButton.addEventListener("click", cardTrash); // Вешаем на "корзину" и "лайк" слушателя и вызываем соответствующую функцию 
     like.addEventListener("click", likeActive);
-    return elCard;
+    photoCard.addEventListener("click", function(){ //вешаем слушателя на фото, получем параметры и сразу пишем в модалку
+    modalPhoto.src = photoCard.src;
+    modalSignature.textContent = nameText.textContent;
+    
+    modalToggle(modalLookPhoto); //открываем - закрываем модалку
+  
+});
+ 
 }
+
 const cardTrash = event =>{
     event.target.closest(".cards").remove(); // ищем ближайшую карточку и убиваем её 
 }
@@ -82,24 +104,23 @@ cardsList(); // запускаем всё это безобразие напис
 
 // функция открытия закрытия модалки 
 function modalToggle(modalWindow) {
-    // если у модалки есть модификатор modal_open, убираем его     
-    if (modalWindow.classList.contains("modal_open")) {
-        modalWindow.classList.toggle("modal_open");
+    if (modalWindow.classList.contains("modal_open")) { // если у модалки есть модификатор modal_open,      
+        modalWindow.classList.toggle("modal_open"); //убираем его
+        modalWindow.classList.toggle("modal_close"); //и ставим modal_close
     }
-    // иначе, добавляем
-    else {
-        modalWindow.classList.toggle("modal_open");
+    else {                                               
+        modalWindow.classList.toggle("modal_close"); // иначе убираем close
+        modalWindow.classList.toggle("modal_open");  // и добавляем open
+         
     }
 } 
 
 /* записываем данные из формы в профиль */
 function modalSave(element) {
+    event.preventDefault();
     profileName.textContent = modalName.value;
     profileProfession.textContent = modalProfession.value;
     modalToggle(element); /* записали, а теперь закрыть окно */
-}
-function prevent(event) { //функция антиперезагрузка страницы
-    event.preventDefault();
 }
 
 //блок редактирования профиля
@@ -109,9 +130,9 @@ editProfileButton.addEventListener("click", function () {
     modalToggle(modalProfile);
 });
 
-saveProfile.addEventListener("click", function () {
+saveProfile.addEventListener("submit", function (event) {
+    event.preventDefault();
     modalSave(modalProfile);
-    prevent(event);
 });
 
 closeModalProfile.addEventListener("click", function () {
@@ -122,7 +143,8 @@ closeModalProfile.addEventListener("click", function () {
 newCardButton.addEventListener("click", function () {
     modalToggle(modalAddCard);
 });
-saveCard.addEventListener("click", function () {
+saveCard.addEventListener("submit", function (event) {
+    event.preventDefault();
     const item = cardsGenerate({
         name: modalTitle.value,
         link: modalPlace.value
@@ -131,9 +153,13 @@ saveCard.addEventListener("click", function () {
     modalTitle.value = ""; //после добавления карточки, очищаем поля ввода, чтобы при следующем открытии поля были пустыми
     modalPlace.value = "";
     modalToggle(modalAddCard);
-    prevent(event);
-
+ 
 });
 closeModalAddCard.addEventListener("click", function () {
     modalToggle(modalAddCard);
+});
+
+//блок просмотра фото
+closeModalLook.addEventListener("click", function () {
+    modalToggle(modalLookPhoto);
 });
